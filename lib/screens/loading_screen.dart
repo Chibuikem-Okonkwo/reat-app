307c7986130d151f5322ec04001efe543070c3a7
Chibuikem Okonkwo <okonkwo.chibuikem@yahoo.com>
2023-05-20 23:49:37 +0100

    Add Shared Preferences dependency

--- lib/screens/loading_screen.dart
+++ lib/screens/loading_screen.dart
@@ -1,12 +1,30 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_spinkit/flutter_spinkit.dart';
 
+import 'onboarding_screen.dart';
+
 class LoadingScreen extends StatefulWidget {
   @override
   _LoadingScreenState createState() => _LoadingScreenState();
 }
 
 class _LoadingScreenState extends State<LoadingScreen> {
+
+  @override
+  void initState() {
+    super.initState();
+    changeRoute();
+  }
+
+  void changeRoute() {
+    Future.delayed(Duration(seconds: 2)).then((value) {
+      Navigator.push(
+        context,
+        MaterialPageRoute(builder: (context) => OnboardingScreen()),
+      );
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -25,4 +43,6 @@ class _LoadingScreenState extends State<LoadingScreen> {
       ),
     );
   }
+}
+
 }
