fb46f8d87c795885df032d310cfc6f0b68f6faca
Chibuikem Okonkwo <okonkwo.chibuikem@yahoo.com>
2023-05-22 08:54:04 +0100

    Update Onboarding Screen

--- lib/screens/onboarding_screen.dart
+++ lib/screens/onboarding_screen.dart
@@ -8,59 +8,161 @@ class OnboardingScreen extends StatefulWidget {
 }
 
 class _OnboardingScreenState extends State<OnboardingScreen> {
-  final controller = PageController(initialPage: 0);
+  final PageController _pageController = PageController(initialPage: 0);
+  int _currentPage = 0;
 
-  @override
-  void dispose(){
-    controller.dispose();
-
-    super.dispose();
-  }
+  final List<Widget> _pages = [
+    OnboardingPage(
+      image: 'images/screen1.png',
+      title: 'Welcome to Onboarding App',
+      description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.',
+    ),
+    OnboardingPage(
+      image: 'images/screen2.png',
+      title: 'Discover New Features',
+      description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.',
+    ),
+    OnboardingPage(
+      image: 'images/screen3.png',
+      title: 'Get Started',
+      description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.',
+    ),
+  ];
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: Container(
-        padding: EdgeInsets.only(bottom: 200),
-        child: PageView(
-          controller: controller,
+        child: Column(
           children: [
-            Container(
-              child: Column(
-                mainAxisAlignment: MainAxisAlignment.center,
-                crossAxisAlignment: CrossAxisAlignment.center,
-                children: [
-                  Image.asset('images/screen1'),
-                  Row(
-                    children: [
-                      Container(
-                        height: 12,
-                        width: 4,
-                        decoration: BoxDecoration(
-                          borderRadius: BorderRadius.all(Radius.circular(12)),
-                        ),
-                      )
-                    ],
-                  ),
-                  Text('Get Your Reservations Easily and Efficiently'),
-                  Text('Browse time convenient for you and keep a spot'),
-                  SizedBox(
-                    child: Column(
-                      children: [
-                        TextButton(onPressed: (){
-                          controller.nextPage(duration: Duration(milliseconds: 300), curve: Curves.ease);
-                        },
-                          child: Text('Next'),
-                        )
-                      ],
-                    ),
-                  ),
-                ],
+            Expanded(
+              child: PageView.builder(
+                controller: _pageController,
+                itemCount: _pages.length,
+                onPageChanged: (int page) {
+                  setState(() {
+                    _currentPage = page;
+                  });
+                },
+                itemBuilder: (context, index) {
+                  return _pages[index];
+                },
               ),
             ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: _buildPageIndicator(),
+            ),
+            SizedBox(height: 16.0),
+            ElevatedButton(
+              onPressed: () {
+                if (_currentPage == _pages.length - 1) {
+                  // Handle "Get Started" button tap
+                  // For example, navigate to the home screen
+                  Navigator.pushReplacement(
+                    context,
+                    MaterialPageRoute(builder: (context) => HomeScreen()),
+                  );
+                } else {
+                  _pageController.nextPage(
+                    duration: Duration(milliseconds: 500),
+                    curve: Curves.ease,
+                  );
+                }
+              },
+              child: Text(
+                _currentPage == _pages.length - 1 ? 'Get Started' : 'Next',
+              ),
+            ),
+            SizedBox(height: 32.0),
           ],
         ),
       ),
     );
   }
+
+  List<Widget> _buildPageIndicator() {
+    List<Widget> indicators = [];
+    for (int i = 0; i < _pages.length; i++) {
+      indicators.add(
+        i == _currentPage
+            ? _indicator(true)
+            : _indicator(false),
+      );
+    }
+    return indicators;
+  }
+
+  Widget _indicator(bool isActive) {
+    return AnimatedContainer(
+      duration: Duration(milliseconds: 150),
+      margin: EdgeInsets.symmetric(horizontal: 8.0),
+      height: 8.0,
+      width: isActive ? 24.0 : 16.0,
+      decoration: BoxDecoration(
+        color: isActive ? Colors.blue : Colors.grey,
+        borderRadius: BorderRadius.all(Radius.circular(12)),
+      ),
+    );
+  }
 }
+
+class OnboardingPage extends StatelessWidget {
+  final String image;
+  final String title;
+  final String description;
+
+  OnboardingPage({
+    required this.image,
+    required this.title,
+    required this.description,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      padding: EdgeInsets.all(32.0),
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Image.asset(image),
+          SizedBox(height: 32.0),
+          Text(
+            title,
+            style: TextStyle(
+              fontSize: 24.0,
+              fontWeight: FontWeight.bold,
+            ),
+            textAlign: TextAlign.center,
+          ),
+          SizedBox(height: 16.0),
+          Text(
+            description,
+            style: TextStyle(
+              fontSize: 16.0,
+            ),
+            textAlign: TextAlign.center,
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+class HomeScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Home Screen'),
+      ),
+      body: Center(
+        child: Text('Welcome to the Home Screen!'),
+      ),
+    );
+  }
+}
+
+
+
+
